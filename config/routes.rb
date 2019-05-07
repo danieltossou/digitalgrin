413e88825b2fcfe0c5af6b12181873a0c160d4c5
danieltossou <oga.lebannen@gmail.com>
2019-05-07 03:43:20 +0000

    Set sign_up page as route root Custumize sign_up page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   resources :messages
   resources :fields
   devise_for :users
+    devise_scope :user do
+      authenticated :user do
+        root :to => 'fields#index'
+      end
+      unauthenticated :user do
+        root :to => 'devise/registrations#new', as: :unauthenticated_root
+      end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
